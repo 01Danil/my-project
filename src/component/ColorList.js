69a16455f5000659ad6419610fc7542e8fec0b76
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-06 12:09:26 +0000

    Add color removal with state change

--- src/component/ColorList.js
+++ src/component/ColorList.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Color from "./Color";
 
-export default function ColorList({ colors = [] }) {
-  if (!colors.length) return <div>No Colors listed</div>;
+export default function ColorList({ colors = [], onRemoveColor = (f) => f }) {
+  if (!colors.length) return <div>No Colors listed. (Add a color)</div>;
   return (
     <div>
       {colors.map((color) => (
-        <Color key={color.id} {...color} />
+        <Color key={color.id} {...color} onRemove={onRemoveColor} />
       ))}
     </div>
   );
