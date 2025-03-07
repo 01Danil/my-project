affd59867ee61f82351976c8ad179a1dc8018b26
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-07 15:08:23 +0000

    Adding colors to the state page 139

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { useState } from "react";
 import colorData from "./color-data.json"; // Импортируем данные
 import ColorList from "./component/ColorList"; // Импортируем компонент ColorList
+import AddColorForm from "./component/AddColorForm";
+import { v4 } from "uuid";
 
 export default function App() {
   const [colors, setColors] = useState(colorData); // Делаем состояние для цветов
 
   return (
-    <ColorList
-      colors={colors}
-      onRateColor={(id, rating) => {
-        // Обновляем рейтинг для выбранного цвета
-        const newColors = colors.map((color) =>
-          color.id === id ? { ...color, rating } : color
-        );
-        setColors(newColors); // Обновляем состояние с новыми данными
-      }}
-      onRemoveColor={(id) => {
-        // Удаляем цвет по id
-        const newColors = colors.filter((color) => color.id !== id);
-        setColors(newColors); // Обновляем состояние с новым списком цветов
-      }}
-    />
+    <>
+      <AddColorForm
+        onNewColor={(title, color) => {
+          const newColors = [...colors, { id: v4(), rating: 0, title, color }];
+          setColors(newColors);
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRateColor={(id, rating) => {
+          // Обновляем рейтинг для выбранного цвета
+          const newColors = colors.map((color) =>
+            color.id === id ? { ...color, rating } : color
+          );
+          setColors(newColors); // Обновляем состояние с новыми данными
+        }}
+        onRemoveColor={(id) => {
+          // Удаляем цвет по id
+          const newColors = colors.filter((color) => color.id !== id);
+          setColors(newColors); // Обновляем состояние с новым списком цветов
+        }}
+      />
+    </>
   );
 }
