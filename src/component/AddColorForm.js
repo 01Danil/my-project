3ce9b6d7f9ebb19ef92d2d2e99f2d61e96bc66a5
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-07 13:35:36 +0000

    Add exemple useInput page 138

--- src/component/AddColorForm.js
+++ src/component/AddColorForm.js
@@ -1,22 +1,26 @@
 import React, { useRef } from "react";
+import { useInput } from "./UseInput";
 
 export default function AddColorForm({ onNewColor = (f) => f }) {
-  const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
-  const submit = (e) => {
-    e.preventDefault();
-    const title = txtTitle.current.value;
-    const color = hexColor.current.value;
-    onNewColor(title, color);
-    txtTitle.current.value = "";
-    hexColor.current.value = "";
+  const submit = (event) => {
+    event.preventDefault();
+    onNewColor(titleProps.value, colorProps.value);
+    resetTitle();
+    resetColor();
   };
 
   return (
     <form onSubmit={submit}>
-      <input ref={txtTitle} type="text" placeholder="color title..." required />
-      <input ref={hexColor} type="color" required />
+      <input
+        {...titleProps}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input {...colorProps} type="color" required />
       <button>ADD</button>
     </form>
   );
