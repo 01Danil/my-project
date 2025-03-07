495edb6ec1624f21800a33c935896fae6ea9c530
Danil Kucheryavko <danil.kucheryavko@gmail.com>
2025-03-07 12:02:37 +0000

    Add controlled components page 136

--- src/component/AddColorForm.js
+++ src/component/AddColorForm.js
@@ -2,7 +2,7 @@ import React, { useRef } from "react";
 
 export default function AddColorForm({ onNewColor = (f) => f }) {
   const [title, setTitle] = useState("");
-	const [color, setColor] = useState("#000000");
+  const [color, setColor] = useState("#000000");
 
   const submit = (e) => {
     e.preventDefault();
@@ -21,3 +21,27 @@ export default function AddColorForm({ onNewColor = (f) => f }) {
     </form>
   );
 }
+
+<form onSubmit={submit}>
+  <input
+    value={title}
+    onChange={(event) => setTitle(event.target.value)}
+    type="text"
+    placeholder="color title..."
+    required
+  />
+  <input
+    value={color}
+    onChange={(event) => setColor(event.target.value)}
+    type="color"
+    required
+  />
+  <button>ADD</button>
+</form>;
+
+const submit = (e) => {
+  e.preventDefault();
+  onNewColor(title, color);
+  setTitle("");
+  setColor("");
+};
